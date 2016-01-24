e9acc4e68fa72f55527665ad925c92c3b568db70
akshay <akshaygupta19221@gmail.com>
2016-01-24 12:35:59 +0530

    first.py added

---
+++ first.py
@@ -0,0 +1,12 @@
+print " Welcome to Glee" "\n  choose your option" 
+print "1 for login"
+print "2 for signup"
+print "3 for exit"
+Option = raw_input()
+if Option == '1' :
+	print " username " "\n password"
+if Option == '2':
+	print " email id"
+if Option == '3' :
+	print "thank you"
+	git tutorial
